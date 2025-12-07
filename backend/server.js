9e37bfa9e24925d13ed518925dde11237d62940e
Stepan Dordiai <stepandordiai@gmail.com>
2025-12-07 20:08:31 +0100

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import { connectDB } from "./config/db.js";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
